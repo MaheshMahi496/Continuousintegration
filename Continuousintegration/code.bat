7a63a8d19e8ae826a10eaf984f30692d00ab651b
Mahesh Rasakonda <mahesh.rasakonda@int.whishworks.com>
2019-07-01 18:28:46 +0530

    First Commit

---
+++ Continuousintegration/code.bat
@@ -0,0 +1,12 @@
+cd D:\SampleDemoProjectMule
+::switch to branch you want to use
+git init
+git checkout master
+::add all added/modified files
+git add .
+
+git commit -am "Add new text"
+::push to git remote repository
+
+git push --set-upstream "https://github.com/MaheshMahi496/CTRepository.git" master
+Pause
